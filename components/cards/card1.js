60a58a784cb8f3b808246aab858b2136b6df5e6a
Xavier <xaviermayora20@gmail.com>
2023-04-03 15:04:55 -0400

    agregando ruta id medicamentos

--- components/cards/card1.js
+++ components/cards/card1.js
@@ -1,9 +1,13 @@
 import { ConfigProvider,Typography, Button, theme } from "antd";
 import { ShoppingCartOutlined } from "@ant-design/icons";
 import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 const {Title} = Typography
 
 const card1 = ({i}) => {
+  const router = useRouter()
+  console.log(router)
   const {token} = theme.useToken()
   return (
     <div
@@ -27,21 +31,23 @@ const card1 = ({i}) => {
           BS. {i.price}
         </Title>
      <ConfigProvider theme={{token:{}}}>
-         <Button
-           size="large"
-           type="primary"
-           style={{
-             width: "100%",
-             lineHeight: "100%",
-             display: "flex",
-             justifyContent: "center",
-             gap: "10px",
-             alignItems: "center",
-           }}
-         >
-           <ShoppingCartOutlined />
-           Anadir
-         </Button>
+         <Link href={`${router.asPath}/${i.title}`}>
+          <Button
+            size="large"
+            type="primary"
+            style={{
+              width: "100%",
+              lineHeight: "100%",
+              display: "flex",
+              justifyContent: "center",
+              gap: "10px",
+              alignItems: "center",
+            }}
+          >
+            <ShoppingCartOutlined />
+            Anadir
+          </Button>
+         </Link>
      </ConfigProvider>
       </ConfigProvider>
     </div>
---
+++ pages/medicamentos/[id]/[medicamento]/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const index = () => {
+    return (
+        <div>
+           funciono 
+        </div>
+    );
+};
+
+export default index;
